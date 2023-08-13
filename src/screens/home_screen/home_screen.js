6364eb6524d7b7fc59b2be87689fdc4823e77e4a
SheCho123 <111002081+SheCho456@users.noreply.github.com>
2023-08-13 03:23:38 +0200

    Sidebar and search bar prototype

--- src/screens/home_screen/home_screen.js
+++ src/screens/home_screen/home_screen.js
@@ -6,10 +6,24 @@ export default function Home_Screen() {
 
     return (
       <div id="mainContainer">
-        <section id="sidebarContainer">
-          <div className="logo" />
-        </section>
+        <div id="sidebarContainer">
+          <div className="logoContainer">
+            <div id="logo">
+              <div id="logoHider"></div>
+            </div>
+          </div>
+        </div>
         <div className="nameButtonContainer">
+          <section className="searchGroup">
+            <select id="regionSelect" placeholder="Region">
+              <option selected disabled hidden>Region:</option>
+              <option>EUNE</option>
+              <option>EUW</option>
+              <option>Korea</option>
+              <option>NA</option>
+            </select>
+            <input placeholder="Search..." id="searchBar"/>
+          </section>
           <section className="buttonGroup1">
             <button>SheCho</button>
             <button>Jeno</button>
